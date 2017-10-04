0265a1b3b46c6e26e7bbddde16acb380af0662dd
Jaco Greeff <jaco@parity.io>
2017-10-04 09:41:48 +0200

    Update release script to use js-precompiled ewn branch

--- js/scripts/release.sh
+++ js/scripts/release.sh
@@ -28,16 +28,16 @@ rm -rf ./.git
 git init
 setup_git_user
 
-echo "*** Checking out $BRANCH branch"
+echo "*** Checking out js-precompiled#ewn branch"
 git remote add origin $GIT_JS_PRECOMPILED
 git fetch origin 2>$GITLOG
-git checkout -b $BRANCH
+git checkout -b ewn
 
 echo "*** Committing compiled files for $UTCDATE"
 mv build ../build.new
 git add .
 git commit -m "$UTCDATE [update]"
-git merge origin/$BRANCH -X ours --commit -m "$UTCDATE [merge]"
+git merge origin/ewn -X ours --commit -m "$UTCDATE [merge]"
 git rm -r build
 rm -rf build
 git commit -m "$UTCDATE [cleanup]"
@@ -46,7 +46,7 @@ git add .
 git commit -m "$UTCDATE [release]"
 
 echo "*** Merging remote"
-git push origin HEAD:refs/heads/$BRANCH 2>$GITLOG
+git push origin HEAD:refs/heads/ewn 2>$GITLOG
 PRECOMPILED_HASH=`git rev-parse HEAD`
 
 # move to root
@@ -64,32 +64,12 @@ if [ "$BRANCH" == "master" ]; then
   npm --no-git-tag-version version
   npm version patch
 
-  echo "*** Building packages for npmjs"
-  echo "$NPM_TOKEN" >> ~/.npmrc
-
-  # build jsonrpc
-  echo "*** Building JSONRPC .json"
-  mkdir -p .npmjs/jsonrpc
-  npm run ci:build:jsonrpc
-
-  for PACKAGE in ${PACKAGES[@]}
-  do
-    echo "*** Building $PACKAGE"
-    LIBRARY=$PACKAGE npm run ci:build:npm
-    DIRECTORY=.npmjs/$PACKAGE
-
-    echo "*** Publishing $PACKAGE from $DIRECTORY"
-    cd $DIRECTORY
-    npm publish --access public || true
-    cd ../..
-  done
-
   cd ..
 fi
 
 echo "*** Updating cargo parity-ui-precompiled#$PRECOMPILED_HASH"
 git submodule update
-sed -i "/^parity-ui-precompiled/ { s/branch = \".*\"/branch = \"$BRANCH\"/g; }" dapps/ui/Cargo.toml
+sed -i "/^parity-ui-precompiled/ { s/branch = \".*\"/branch = \"ewn\"/g; }" dapps/ui/Cargo.toml
 cargo update -p parity-ui-precompiled
 # --precise "$PRECOMPILED_HASH"
 
